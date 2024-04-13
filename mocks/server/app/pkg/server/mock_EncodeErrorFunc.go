cbe993d0f5497e03cbf024f69c83128ec194dcfd
bonavi <124391691+bonavi@users.noreply.github.com>
2024-04-13 21:14:15 +0700

    fix failed test

--- mocks/server/app/pkg/server/mock_EncodeErrorFunc.go
+++ mocks/server/app/pkg/server/mock_EncodeErrorFunc.go
@@ -22,9 +22,9 @@ func (_m *MockEncodeErrorFunc) EXPECT() *MockEncodeErrorFunc_Expecter {
 	return &MockEncodeErrorFunc_Expecter{mock: &_m.Mock}
 }
 
-// Execute provides a mock function with given fields: _a0, _a1, _a2, _a3
-func (_m *MockEncodeErrorFunc) Execute(_a0 context.Context, _a1 http.ResponseWriter, _a2 error, _a3 func(error)) {
-	_m.Called(_a0, _a1, _a2, _a3)
+// Execute provides a mock function with given fields: _a0, _a1, _a2
+func (_m *MockEncodeErrorFunc) Execute(_a0 context.Context, _a1 http.ResponseWriter, _a2 error) {
+	_m.Called(_a0, _a1, _a2)
 }
 
 // MockEncodeErrorFunc_Execute_Call is a *mock.Call that shadows Run/Return methods with type explicit version for method 'Execute'
@@ -36,14 +36,13 @@ type MockEncodeErrorFunc_Execute_Call struct {
 //   - _a0 context.Context
 //   - _a1 http.ResponseWriter
 //   - _a2 error
-//   - _a3 func(error)
-func (_e *MockEncodeErrorFunc_Expecter) Execute(_a0 interface{}, _a1 interface{}, _a2 interface{}, _a3 interface{}) *MockEncodeErrorFunc_Execute_Call {
-	return &MockEncodeErrorFunc_Execute_Call{Call: _e.mock.On("Execute", _a0, _a1, _a2, _a3)}
+func (_e *MockEncodeErrorFunc_Expecter) Execute(_a0 interface{}, _a1 interface{}, _a2 interface{}) *MockEncodeErrorFunc_Execute_Call {
+	return &MockEncodeErrorFunc_Execute_Call{Call: _e.mock.On("Execute", _a0, _a1, _a2)}
 }
 
-func (_c *MockEncodeErrorFunc_Execute_Call) Run(run func(_a0 context.Context, _a1 http.ResponseWriter, _a2 error, _a3 func(error))) *MockEncodeErrorFunc_Execute_Call {
+func (_c *MockEncodeErrorFunc_Execute_Call) Run(run func(_a0 context.Context, _a1 http.ResponseWriter, _a2 error)) *MockEncodeErrorFunc_Execute_Call {
 	_c.Call.Run(func(args mock.Arguments) {
-		run(args[0].(context.Context), args[1].(http.ResponseWriter), args[2].(error), args[3].(func(error)))
+		run(args[0].(context.Context), args[1].(http.ResponseWriter), args[2].(error))
 	})
 	return _c
 }
@@ -53,7 +52,7 @@ func (_c *MockEncodeErrorFunc_Execute_Call) Return() *MockEncodeErrorFunc_Execut
 	return _c
 }
 
-func (_c *MockEncodeErrorFunc_Execute_Call) RunAndReturn(run func(context.Context, http.ResponseWriter, error, func(error))) *MockEncodeErrorFunc_Execute_Call {
+func (_c *MockEncodeErrorFunc_Execute_Call) RunAndReturn(run func(context.Context, http.ResponseWriter, error)) *MockEncodeErrorFunc_Execute_Call {
 	_c.Call.Return(run)
 	return _c
 }
