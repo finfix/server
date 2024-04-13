3f0ec73604d35c155b0cee58f7564dd6b4ac0f2a
bonavi <124391691+bonavi@users.noreply.github.com>
2024-04-13 20:45:53 +0700

    add update currencies in swagger

--- app/services/admin/endpoint/updateCurrencies.go
+++ app/services/admin/endpoint/updateCurrencies.go
@@ -5,6 +5,12 @@ import (
 	"net/http"
 )
 
+// @Summary Обновление курсов валют
+// @Tags admin
+// @Security SecretKey
+// @Success 200 "При успешном выполнении возвращается пустой ответ"
+// @Failure 400,401,403,500 {object} errors.CustomError
+// @Router /admin/updateCurrencies [post]
 func (s *endpoint) updateCurrencies(ctx context.Context, _ *http.Request) (any, error) {
 	return nil, s.service.UpdateCurrencies(ctx)
 }
