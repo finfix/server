42fe5d37c13fe3fa07bce60d9d8ba39ae99729d6
bonavi <124391691+bonavi@users.noreply.github.com>
2024-05-10 04:10:22 +0700

    add fields for change remainder on creating accounts

--- app/services/account/model/response.go
+++ app/services/account/model/response.go
@@ -5,14 +5,16 @@ type GetRes struct {
 }
 
 type CreateAccountRes struct {
-	ID                     uint32  `json:"id"`                                         // Идентификатор созданного счета
-	SerialNumber           uint32  `json:"serialNumber"`                               // Порядковый номер счета
-	BalancingTransactionID *uint32 `json:"balancingTransactionID" validate:"required"` // Идентификатор транзакции балансировки
+	ID                           uint32  `json:"id"`                                               // Идентификатор созданного счета
+	SerialNumber                 uint32  `json:"serialNumber"`                                     // Порядковый номер счета
+	BalancingTransactionID       *uint32 `json:"balancingTransactionID" validate:"required"`       // Идентификатор транзакции балансировки
+	BalancingAccountID           *uint32 `json:"balancingAccountID" validate:"required"`           // Идентификатор балансировочного счета
+	BalancingAccountSerialNumber *uint32 `json:"balancingAccountSerialNumber" validate:"required"` // Порядковый номер балансировочного счета
 }
 
 type UpdateAccountRes struct {
-	BalancingTransactionID       *uint32 `json:"balancingTransactionID" validate:"required"`       // Идентификатор транзакции
 	BalancingAccountID           *uint32 `json:"balancingAccountID" validate:"required"`           // Идентификатор балансировочного счета
+	BalancingTransactionID       *uint32 `json:"balancingTransactionID" validate:"required"`       // Идентификатор транзакции
 	BalancingAccountSerialNumber *uint32 `json:"balancingAccountSerialNumber" validate:"required"` // Порядковый номер балансировочного счета
 }
 
